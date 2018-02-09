57c60c3d3f96905cb94942b20d08246b52c625e7
jihengwang <jiw367@ucsd.edu>
2018-02-08 21:09:12 -0800

    The final version finished

--- Titan/src/db/mongodb/MongoDBTableCreation.java
+++ Titan/src/db/mongodb/MongoDBTableCreation.java
@@ -1,7 +1,10 @@
 package db.mongodb;
 
+import org.bson.Document;
+
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.IndexOptions;
 import com.sun.org.apache.xerces.internal.impl.xpath.regex.ParseException;
 
 public class MongoDBTableCreation {
@@ -9,6 +12,24 @@ public class MongoDBTableCreation {
 	public static void main(String[] args) throws ParseException {
 	    MongoClient mongoClient = new MongoClient();
 	    MongoDatabase db = mongoClient.getDatabase(MongoDBUtil.DB_NAME);
+
+		// Step 1: remove old collections.
+		db.getCollection("users").drop();
+		db.getCollection("items").drop();
+
+		// Step 2: populate data and create index.
+		db.getCollection("users").insertOne(new Document().append("first_name", "John").append("last_name", "Smith")
+				.append("password", "3229c1097c00d497a0fd282d586be050").append("user_id", "1111"));
+		// make sure user_id is unique.
+		IndexOptions indexOptions = new IndexOptions().unique(true);
+
+		// use 1 for ascending index , -1 for descending index
+		// Different to MySQL, users table in MongoDB also has history info.
+		db.getCollection("users").createIndex(new Document("user_id", 1), indexOptions);
+
+		// make sure item_id is unique.
+		// Different to MySQL, items table in MongoDB also has categories info.
+		db.getCollection("items").createIndex(new Document("item_id", 1), indexOptions);
 
 	    mongoClient.close();
 	    System.out.println("Import is done successfully.");
